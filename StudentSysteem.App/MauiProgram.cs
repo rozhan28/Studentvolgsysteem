a0f03db8a2eec78ff244ed406bce3d3c8c618181
rozhan28 <rozhanarya1@gmail.com>
2025-11-21 23:48:11 +0100

    Behoud van formulierinvoer toegevoegd

--- StudentSysteem.App/MauiProgram.cs
+++ StudentSysteem.App/MauiProgram.cs
@@ -1,24 +1,37 @@
 ﻿using Microsoft.Extensions.Logging;
+using StudentSysteem.App.ViewModels;
+using StudentSysteem.App.Views;
+using StudentVolgSysteem.Core.Services;
 
 namespace StudentSysteem.App;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+        // Services
+        builder.Services.AddSingleton<ISelfReflectionService, MockSelfReflectionService>();
+
+        // ViewModels (blijven bestaan)
+        builder.Services.AddSingleton<FeedbackFormViewModel>();
+
+        
+        builder.Services.AddSingleton<StartView>();
+        builder.Services.AddSingleton<FeedbackFormView>();
 
 #if DEBUG
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }
