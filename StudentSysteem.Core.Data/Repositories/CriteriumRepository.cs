0f1e663b286bf252f1b08a7acf488eb9a996cf96
rozhan28 <rozhanarya1@gmail.com>
2025-12-16 15:53:51 +0100

    bug opgelost

--- StudentSysteem.Core.Data/Repositories/CriteriumRepository.cs
+++ StudentSysteem.Core.Data/Repositories/CriteriumRepository.cs
@@ -18,14 +18,14 @@ namespace StudentSysteem.Core.Data.Repositories
         {
              //Op niveau
             @"INSERT OR IGNORE INTO Criterium (criterium_id, beschrijving)
-              VALUES (2, 'De syntax van het domeinmodel is correct volgens UML')",
+              VALUES (1, 'De syntax van het domeinmodel is correct volgens UML')",
 
             @"INSERT OR IGNORE INTO Criterium (criterium_id, beschrijving)
-              VALUES (3, 'Het domeinmodel is op een logische locatie vastgelegd')",
+              VALUES (2, 'Het domeinmodel is op een logische locatie vastgelegd')",
 
             //Boven niveau
             @"INSERT OR IGNORE INTO Criterium (criterium_id, beschrijving)
-              VALUES (4, 'Het domeinmodel is volledig en logisch')" 
+              VALUES (3, 'Het domeinmodel is volledig en logisch')" 
         };
 
             VoegMeerdereInMetTransactie(seed);
