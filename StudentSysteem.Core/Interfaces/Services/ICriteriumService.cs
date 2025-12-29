2185c5e6f4771c7b34166308a4472b5cf7a4b6bf
rozhan28 <rozhanarya1@gmail.com>
2025-12-29 01:42:45 +0100

    test

--- StudentSysteem.Core/Interfaces/Services/ICriteriumService.cs
+++ StudentSysteem.Core/Interfaces/Services/ICriteriumService.cs
@@ -9,5 +9,5 @@ namespace StudentSysteem.Core.Interfaces.Services
     {
         List<Criterium> HaalOpNiveauCriteriaOp();
         List<Criterium> HaalBovenNiveauCriteriaOp();
-    }
+    }//test
 }
