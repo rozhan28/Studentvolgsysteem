787bd46a73e615bb445fc967e042b520a26f6bb6
Xander <s1218452@student.windesheim.nl>
2025-12-30 10:37:58 +0100

    Leeruitkomst Model gemaakt, WIP, niet zeker over HboActiviteit omdat dit het zelfde is zoals in vaardigheid.

---
+++ StudentSysteem.Core/Models/Leeruitkomst.cs
@@ -0,0 +1,19 @@
+﻿namespace StudentSysteem.Core.Models
+{
+    public class Leeruitkomst
+    {
+        public int Leeruitkomst_id { get; set; }
+        public string LeeruitkomstNaam { get; set; }
+        public string HboiActiviteit { get; set; }
+        public int Datapunt_id { get; set; }
+
+        public Leeruitkomst(int leeruitkomst_id, string leeruitkomstNaam, string hboiActiviteit, int datapunt_id)
+        {
+            Leeruitkomst_id = leeruitkomst_id;
+            LeeruitkomstNaam = leeruitkomstNaam;
+            HboiActiviteit = hboiActiviteit;
+            Datapunt_id = datapunt_id;
+        }
+
+    }
+}
