79e48d70d87bb7be4559a00432d0070a61f13119
Xander <s1218452@student.windesheim.nl>
2025-12-30 10:26:30 +0100

    Model Datapunt gemaakt

---
+++ StudentSysteem.Core/Models/Datapunt.cs
@@ -0,0 +1,16 @@
+﻿namespace StudentSysteem.Core.Models
+{
+    public class Datapunt
+    {
+        public int Datapunt_id { get; set; }
+        public string DatapuntNaam { get; set; }
+        public string Leeruitkomst_id { get; set; }
+
+        public Datapunt(int datapunt_id, string datapuntNaam, string leeruitkomst_id)
+        {
+            Datapunt_id = datapunt_id;
+            DatapuntNaam = datapuntNaam;
+            Leeruitkomst_id = leeruitkomst_id;
+        }
+    }
+}
