752e593e77cab971cd775328537be7b4c433eff3
Xander <s1218452@student.windesheim.nl>
2026-01-02 14:55:44 +0100

    Vaardigheid Model terug gezet naar origineel.

--- StudentSysteem.Core/Models/Vaardigheid.cs
+++ StudentSysteem.Core/Models/Vaardigheid.cs
@@ -2,28 +2,22 @@
 {
     public class Vaardigheid
     {
-        public int Id { get; set; }
-        public string Naam { get; set; }
-        public string Beschrijving { get; set; }
-        public string LeertakenUrl { get; set; }
-        public int PrestatiedoelId { get; set; }
-        public int ProcesstapId { get; set; }
-        public int LeeruitkomstId { get; set; }
+        public int Vaardigheid_id { get; set; }
+        public string VaardigheidNaam { get; set; }
+        public string VaardigheidBeschrijving { get; set; }
         public string HboiActiviteit { get; set; }
+        public string LeertakenUrl { get; set; }
+        public int Prestatiedoel_id { get; set; }
+        public int Processtap_id { get; set; }
 
-        public Vaardigheid(int id, string naam, string beschrijving, string leertakenUrl,
-                           int prestatiedoelId, int leeruitkomstId, string hboiActiviteit)
+        public Vaardigheid(int vaardigheid_id, string vaardigheidNaam, string vaardigheidBeschrijving, string hboiActiviteit, string leertakenUrl, int prestatiedoelId)
         {
-            Id = id;
-            Naam = naam;
-            Beschrijving = beschrijving;
-            LeertakenUrl = leertakenUrl;
-            PrestatiedoelId = prestatiedoelId;
-            LeeruitkomstId = leeruitkomstId;
+            Vaardigheid_id = vaardigheid_id;
+            VaardigheidNaam = vaardigheidNaam;
+            VaardigheidBeschrijving = vaardigheidBeschrijving;
             HboiActiviteit = hboiActiviteit;
+            LeertakenUrl = leertakenUrl;
+            Prestatiedoel_id = prestatiedoelId;
         }
-
-        public string VaardigheidNaam => Naam;
-        public string VaardigheidBeschrijving => Beschrijving;
     }
 }
