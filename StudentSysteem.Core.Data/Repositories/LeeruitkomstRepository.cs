a752b338a659699b370dd8b97a86aea3a7f07194
g4g2edsf <s1205590@student.windesheim.nl>
2026-01-02 13:22:45 +0100

    Code van mij en dev samengevoegd om leeruitkomst goed te laten werken

--- StudentSysteem.Core.Data/Repositories/LeeruitkomstRepository.cs
+++ StudentSysteem.Core.Data/Repositories/LeeruitkomstRepository.cs
@@ -1,4 +1,5 @@
-﻿using StudentSysteem.Core.Data.Helpers;
+﻿using Microsoft.Data.Sqlite;
+using StudentSysteem.Core.Data.Helpers;
 using StudentSysteem.Core.Interfaces.Repository;
 using StudentSysteem.Core.Models;
 
@@ -9,36 +10,48 @@ namespace StudentSysteem.Core.Data.Repositories
         public LeeruitkomstRepository(DbConnectieHelper dbConnectieHelper) : base(dbConnectieHelper)
         {
             MaakTabel(@"CREATE TABLE IF NOT EXISTS Leeruitkomst (
-                    [leeruitkomst_id] INTEGER PRIMARY KEY AUTOINCREMENT,
-                    [naam] VARCHAR(50),
-                    [hboi_activiteit] VARCHAR(50))");
+                    leeruitkomst_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    naam VARCHAR(50),
+                    hboi_activiteit VARCHAR(50))");
 
-            List<string> insertQueries = [@"INSERT OR REPLACE INTO Leeruitkomst(leeruitkomst_id, naam, hboi_activiteit) 
-                                        VALUES(NULL, 'MockData2', NULL)"];
+            List<string> insertQueries = new()
+            {
+                @"INSERT OR IGNORE INTO Leeruitkomst(leeruitkomst_id, naam, hboi_activiteit) 
+                  VALUES(1, 'Analyseren', 'Analyseren')",
+                @"INSERT OR IGNORE INTO Leeruitkomst(leeruitkomst_id, naam, hboi_activiteit) 
+                  VALUES(2, 'Ontwerpen', 'Ontwerpen')"
+            };
             VoegMeerdereInMetTransactie(insertQueries);
+
         }
-        
+
+        public Leeruitkomst? HaalOp()
+        {
+            List<Leeruitkomst> alleLeeruitkomsten = HaalAlleLeeruitkomstenOp();
+            return alleLeeruitkomsten.FirstOrDefault();
+        }
+
         public List<Leeruitkomst> HaalAlleLeeruitkomstenOp()
         {
-            List<Leeruitkomst> lijst = new();
-
+            List<Leeruitkomst> leeruitkomsten = new();
+            leeruitkomsten.Clear();
+            string selectQuery = "SELECT leeruitkomst_id, naam, hboi_activiteit FROM Leeruitkomst";
             OpenVerbinding();
-            using var cmd = Verbinding.CreateCommand();
-            cmd.CommandText = @"SELECT leeruitkomst_id, naam, hboi_activiteit FROM Leeruitkomst";
 
-            using var reader = cmd.ExecuteReader();
-            while (reader.Read())
+            using (SqliteCommand command = new(selectQuery, Verbinding))
             {
-                lijst.Add(new Leeruitkomst
-                {
-                    Leeruitkomst_id = reader.GetInt32(0),
-                    LeeruitkomstNaam = reader.GetString(1),
-                    HboiActiviteit = reader.GetString(2),
-                });
-            }
+                SqliteDataReader reader = command.ExecuteReader();
 
+                while (reader.Read())
+                {
+                    int LeeruitkomstId = reader.GetInt32(0);
+                    string LeeruitkomstNaam = reader.GetString(1);
+                    string HboiActiviteit = reader.GetString(2);
+                    leeruitkomsten.Add(new(LeeruitkomstId, LeeruitkomstNaam, HboiActiviteit));
+                }
+            }
             SluitVerbinding();
-            return lijst;
+            return leeruitkomsten;
         }
     }
 }
--- StudentSysteem.Core/Models/Leeruitkomst.cs
+++ StudentSysteem.Core/Models/Leeruitkomst.cs
@@ -5,5 +5,12 @@
         public int Leeruitkomst_id { get; set; }
         public string LeeruitkomstNaam { get; set; }
         public string HboiActiviteit { get; set; }
+
+        public Leeruitkomst(int leeruitkomst_id, string leeruitkomstNaam, string hboiActiviteit)
+        {
+            Leeruitkomst_id = leeruitkomst_id;
+            LeeruitkomstNaam = leeruitkomstNaam;
+            HboiActiviteit = hboiActiviteit;
+        }
     }
 }
