6da6c6ce96e2c2bfa8c7975452d6c8dc819f79ce
Sanne <sanne.abrahamse@windesheim.nl>
2026-01-02 08:56:18 +0100

    Enum niveauaanduiding toegevoegd, converter aangepast.

---
+++ StudentSysteem.App/Converters/PrestatieniveauKleurConverter.cs
@@ -0,0 +1,43 @@
+using System.Globalization;
+using StudentSysteem.Core.Models;
+
+namespace StudentSysteem.App.Converters
+{
+    public class PrestatieniveauKleurConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Niveauaanduiding niveau)
+            {
+                return niveau switch
+                {
+                    Niveauaanduiding.InOntwikkeling => GetColorResource("InOntwikkeling"),
+                    Niveauaanduiding.OpNiveau => GetColorResource("OpNiveau"),
+                    Niveauaanduiding.BovenNiveau => GetColorResource("BovenNiveau"),
+                    _ => GetColorResource("NietIngeleverd")
+                };
+            }
+            return Colors.Transparent;
+        }
+
+        private static Color GetColorResource(string resourceKey)
+        {
+            if (Application.Current?.Resources != null && 
+                Application.Current.Resources.TryGetValue(resourceKey, out object resourceValue))
+            {
+                if (resourceValue is Color color)
+                {
+                    return color;
+                }
+                if (resourceValue is SolidColorBrush brush)
+                {
+                    return brush.Color;
+                }
+            }
+            return Colors.Transparent; 
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+            throw new NotImplementedException();
+    }
+}
---
+++ StudentSysteem.Core/Models/EnumNiveauaanduiding.cs
@@ -0,0 +1,9 @@
+namespace StudentSysteem.Core.Models;
+
+public enum Niveauaanduiding
+{
+    NietIngeleverd = 0,
+    InOntwikkeling = 1,
+    OpNiveau = 2,
+    BovenNiveau = 3
+}
