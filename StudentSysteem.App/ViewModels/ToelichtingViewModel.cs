42cda387cb3ade33e4ce45ef64786467b39ad4d4
rozhan28 <rozhanarya1@gmail.com>
2026-01-04 12:22:09 +0100

    ToelichitingViewModel.cs toegevoegd en bijbhehorende logica toegepast

--- StudentSysteem.App/ViewModels/ToelichtingViewModel.cs
+++ StudentSysteem.App/ViewModels/ToelichtingViewModel.cs
@@ -1,7 +1,71 @@
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using StudentSysteem.Core.Interfaces.Services;
+using StudentSysteem.Core.Models;
+
 namespace StudentSysteem.App.ViewModels;
 
-// Handelt alle toelichtingen af + koppeling toelichting met criteria
-public partial class ToelichtingViewModel : BasisViewModel
+public class ToelichtingViewModel : BasisViewModel
 {
-    
+    private readonly IToelichtingService _toelichtingService;
+    private readonly bool _isDocent;
+
+    public BeoordelingItem Beoordeling { get; }
+
+    public ICommand VoegExtraToelichtingToeCommand { get; }
+
+    public ToelichtingViewModel(
+        BeoordelingItem beoordeling,
+        IToelichtingService toelichtingService,
+        bool isDocent)
+    {
+        Beoordeling = beoordeling;
+        _toelichtingService = toelichtingService;
+        _isDocent = isDocent;
+
+        VoegExtraToelichtingToeCommand =
+            new Command(VoegExtraToelichtingToe);
+
+        HookToelichtingen();
+    }
+
+    // Validatie
+    public bool ZijnAlleToelichtingenOk()
+    {
+        if (_isDocent) return true;
+        if (Beoordeling.Toelichtingen == null || !Beoordeling.Toelichtingen.Any())
+            return false;
+
+        return Beoordeling.Toelichtingen.All(t =>
+            !string.IsNullOrWhiteSpace(t.Tekst) &&
+            t.GeselecteerdeOptie != "Toelichting gekoppeld aan...");
+    }
+
+    // Toevoegen
+    private void VoegExtraToelichtingToe()
+    {
+        if (Beoordeling.Toelichtingen.Count >=
+            _toelichtingService.BerekenMaxToelichtingen(Beoordeling.PrestatiedoelId))
+            return;
+
+        Beoordeling.Toelichtingen.Add(
+            _toelichtingService.MaakNieuweToelichting());
+    }
+
+    private void HookToelichtingen()
+    {
+        UpdateKanToevoegen();
+
+        Beoordeling.Toelichtingen.CollectionChanged += (_, _) =>
+        {
+            UpdateKanToevoegen();
+        };
+    }
+
+    private void UpdateKanToevoegen()
+    {
+        Beoordeling.KanExtraToelichtingToevoegen =
+            Beoordeling.Toelichtingen.Count <
+            _toelichtingService.BerekenMaxToelichtingen(Beoordeling.PrestatiedoelId);
+    }
 }
