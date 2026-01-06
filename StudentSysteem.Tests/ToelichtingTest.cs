315c9f54907eefecff3cabec8fae6afd5cecf526
rozhan28 <rozhanarya1@gmail.com>
2026-01-06 12:51:41 +0100

    Geautomatiseerde test (UC1 / TC1-01.1) : WIP

---
+++ StudentSysteem.Tests/ToelichtingTest.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using Moq;
+using StudentSysteem.Core.Interfaces.Repository;
+using StudentSysteem.Core.Services;
+using StudentSysteem.Core.Models;
+using System.Collections.Generic;
+
+namespace StudentSysteem.Tests
+{
+    [TestFixture]
+    public class ToelichtingTest
+    {
+        private Mock<IFeedbackRepository> _feedbackRepositoryMock;
+        private FormulierService _formulierService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _feedbackRepositoryMock = new Mock<IFeedbackRepository>();
+            _formulierService = new FormulierService(_feedbackRepositoryMock.Object);
+        }
+
+
+        // UC1 / TC1-01.2 – Docent kan toelichting invoeren en opslaan (Happy Path)
+        [Test]
+        public void UC1_HappyPath_ToelichtingWordtOpgeslagen()
+        {
+            // Arrange (Preconditie)
+            var criterium = new Criterium(
+                id: 1,
+                beschrijving: "Samenwerken",
+                niveau: Niveauaanduiding.OpNiveau
+            );
+
+            var toelichting = new Toelichting
+            {
+                Tekst = "Goede samenwerking en duidelijke communicatie",
+                GeselecteerdeOptie = criterium
+            };
+
+            var feedback = new Feedback(vaardigheidId: 1)
+            {
+                StudentId = 10,
+                DocentId = 5,
+                FeedbackGeverId = 5,
+                Toelichtingen = new List<Toelichting> { toelichting }
+            };
+
+            var feedbackLijst = new List<Feedback> { feedback };
+
+            // Act (Stappen)
+            _formulierService.SlaFeedbackOp(feedbackLijst);
+
+            // Assert (Postconditie)
+            _feedbackRepositoryMock.Verify(
+                repo => repo.VoegFeedbackToe(It.IsAny<List<Feedback>>()),
+                Times.Once,
+                "Feedback moet worden opgeslagen wanneer de invoer geldig is"
+            );
+        }
+    }
+}
+
