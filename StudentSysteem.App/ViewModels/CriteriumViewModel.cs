86ca16044f4a43d2c3ec30fa1c9ea237357f9c1b
Jules-Windesheim <s1205590@student.windesheim.nl>
2026-01-06 10:25:36 +0100

    Revert "Niveau toegevoegd bij criteriumviewmodel voor het opslaan van het geselecteerde niveau"
    This reverts commit c366846096ae3b40fca00c458b2bdc3e289f441c.

--- StudentSysteem.App/ViewModels/CriteriumViewModel.cs
+++ StudentSysteem.App/ViewModels/CriteriumViewModel.cs
@@ -18,9 +18,6 @@ public partial class CriteriumViewModel : BasisViewModel
     private readonly ICriteriumService _criteriumService;
     private Prestatiedoel _prestatiedoel;
     public ICommand SelecteerCriteriumCommand { get; }
-
-    public Niveauaanduiding Niveau;
-    
     public ObservableCollection<Criterium> OpNiveauCriteria { get; set; }
     public ObservableCollection<Criterium> BovenNiveauCriteria { get; set; }
     
