808b72cda46ae0de127fcce821431079ed064567
Jules-Windesheim <s1205590@student.windesheim.nl>
2026-01-08 09:33:46 +0100

    argument toegevoegd aan de ValidatieRandkleurConverter.cs zodat de ene validatierand van rood naar transparant kan wisselen en de ander van rood naar grijs

--- StudentSysteem.App/Converters/ValidatieRandkleurConverter.cs
+++ StudentSysteem.App/Converters/ValidatieRandkleurConverter.cs
@@ -7,7 +7,17 @@ namespace StudentSysteem.App.Converters
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             bool isInvalid = (bool)value;
+            string standardColor = parameter as string ?? "Grey";
+            
+            // Bepaal welke Key gezocht moet worden in de Resources
+            string colorKey = isInvalid ? "Validatie" : standardColor;
 
+            if (Application.Current.Resources.TryGetValue(colorKey, out var color))
+            {
+                return (Color)color;
+            }
+
+            // Fallback voor het geval de keys niet gevonden worden in Colors.xaml
             return isInvalid ? Colors.Red : Colors.LightGray;
         }
 
