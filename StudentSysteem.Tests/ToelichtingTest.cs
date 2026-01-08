b6152e1d464b2dc1d2c7d8838a5467ac8b63f389
rozhan28 <rozhanarya1@gmail.com>
2026-01-08 14:58:02 +0100

    Unittest geslaagd en

--- StudentSysteem.Tests/ToelichtingTest.cs
+++ StudentSysteem.Tests/ToelichtingTest.cs
@@ -20,31 +20,7 @@ namespace StudentSysteem.Tests
             _formulierService = new FormulierService(_feedbackRepositoryMock.Object);
         }
 
-        //UC1 / TC1-01.1.3 - Docent kan opslaan zonder toelichting
-        [Test]
-        public void UC1_HappyPath_Docent_ZonderToelichting_WordtOpgeslagen()
-        {
-            Feedback feedback = new Feedback(vaardigheidId: 1)
-            {
-                StudentId = 10,
-                DocentId = 5,              // 👈 DOCENT
-                FeedbackGeverId = 5,
-                Toelichtingen = new List<Toelichting>
-        {
-            new Toelichting { Tekst = "" }
-        }
-            };
-
-            _formulierService.SlaFeedbackOp(new() { feedback });
-
-            _feedbackRepositoryMock.Verify(
-                repo => repo.VoegFeedbackToe(It.IsAny<List<Feedback>>()),
-                Times.Once
-            );
-        }
-
         // UC1 / TC1-01.1.1 – Docent kan toelichting invoeren en opslaan 
-        //test
         [Test]
         public void UC1_HappyPath_ToelichtingWordtOpgeslagen()
         {
@@ -79,8 +55,64 @@ namespace StudentSysteem.Tests
                 repo => repo.VoegFeedbackToe(It.IsAny<List<Feedback>>()),
                 Times.Once,
                 "Feedback moet worden opgeslagen wanneer de invoer geldig is"
-            );            
+            );
         }
+
+        //UC1 / TC1-01.1.3 - Docent kan opslaan zonder toelichting
+        [Test]
+        public void UC1_HappyPath_Docent_ZonderToelichting_WordtOpgeslagen()
+        {
+            Feedback feedback = new Feedback(vaardigheidId: 1)
+            {
+                StudentId = 10,
+                DocentId = 5,              // 👈 DOCENT
+                FeedbackGeverId = 5,
+                Toelichtingen = new List<Toelichting>
+        {
+            new Toelichting { Tekst = "" }
+        }
+            };
+
+            _formulierService.SlaFeedbackOp(new() { feedback });
+
+            _feedbackRepositoryMock.Verify(
+                repo => repo.VoegFeedbackToe(It.IsAny<List<Feedback>>()),
+                Times.Once
+            );
+        }
+
+        // UC1 / TC1-01.4 – Student kan geen toelichting met null tekst opslaan \
+        [Test]
+        public void UC1_UnhappyPath_Student_ToelichtingNull_GooitArgumentException()
+        {
+            // Arrange
+            Toelichting toelichting = new Toelichting
+            {
+                Tekst = null
+            };
+
+            Feedback feedback = new Feedback(vaardigheidId: 1)
+            {
+                StudentId = 10,
+                DocentId = 0,              
+                FeedbackGeverId = 10,
+                Toelichtingen = new List<Toelichting> { toelichting }
+            };
+
+            List<Feedback> feedbackLijst = new() { feedback };
+
+            // Act + Assert
+            Assert.Throws<ArgumentException>(() =>
+                _formulierService.SlaFeedbackOp(feedbackLijst)
+            );
+
+            // Extra controle: feedback wordt niet opgeslagen
+            _feedbackRepositoryMock.Verify(
+                repo => repo.VoegFeedbackToe(It.IsAny<List<Feedback>>()),
+                Times.Never
+            );
+        }
+
     }
 }
 
