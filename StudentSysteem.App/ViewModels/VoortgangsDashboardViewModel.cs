8f0c86c26376e852840b00e6c44f398f1a8e0dda
Xander <s1218452@student.windesheim.nl>
2026-01-09 10:21:08 +0100

    Voortgangsdashboard toegevoegd met logica

---
+++ StudentSysteem.App/ViewModels/VoortgangsDashboardViewModel.cs
@@ -0,0 +1,29 @@
+﻿using StudentSysteem.Core.Interfaces.Services;
+using StudentSysteem.Core.Models;
+using System.Collections.ObjectModel;
+
+namespace StudentSysteem.App.ViewModels
+{
+    public class VoortgangsDashboardViewModel
+    {
+        private readonly ILeeruitkomstService _leeruitkomstService;
+
+        public ObservableCollection<Leeruitkomst> Leeruitkomsten { get; } = new ObservableCollection<Leeruitkomst>();
+
+        public VoortgangsDashboardViewModel(ILeeruitkomstService leeruitkomstService)
+        {
+            _leeruitkomstService = leeruitkomstService;
+            LaadLeeruitkomsten();
+        }
+
+        private void LaadLeeruitkomsten()
+        {
+            IEnumerable<Leeruitkomst> leeruitkomstService = _leeruitkomstService.HaalAlleLeeruitkomstenOp();
+            // Beperk het dashboard om maximaal 5 HBO-i-activiteit te laten zien in de tabel
+            foreach (Leeruitkomst leeruitkomst in leeruitkomstService.Take(5))
+            {
+                Leeruitkomsten.Add(leeruitkomst);
+            }
+        }
+    }
+}
